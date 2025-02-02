a8db4c1ccbb6172a52e4c378c521ab175af8fd78
Luiz Gustavo <luizgustavolb2021@gmail.com>
2025-02-01 23:00:18 -0300

    Função adicionada

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 let amigos = []
-a
+
+
+// consegui aprender como faz uma função e como pegar algo do usuario e colocar em outro lugar... aee
+function adicionarAmigo() {
+
+var a = document.getElementById("amigo").value
+
+console.log(a)
+
+}
+
