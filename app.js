e30000a2ad57786e59dbdc6d14a0a92382f67dfc
Luiz Gustavo <luizgustavolb2021@gmail.com>
2025-02-23 14:30:17 -0300

    Revert "	modified:   app.js"
    This reverts commit 6b704e5ef2fd470fd3e4e6554d62332a2d775677.
    
    	modified:   app.js

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+/*
+        let amigos = []
+
+
+        function adicionarAmigo() {
+
+            //pega o que foi escrito pelo usuario
+        var a = document.getElementById("amigo").value
+
+
+            //#2 fazer uma validação para 
+        if (a != Number || null) {
+        amigos.push(a)
+
+            //coloca com virgula tudo...
+            let b = document.getElementById("listaAmigos");
+            b.innerHTML = amigos
+
+        } else {
+
+            alert('digite algum caractere valido')
+            
+        }
+
+        }
+*/
+
 let amigos = []
 
 
