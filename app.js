126959cf35eb1a3291cc2d7fdc1126221ff9c6b3
Luiz Gustavo <luizgustavolb2021@gmail.com>
2025-02-23 14:26:18 -0300

    Revert "feito a função e feito o condicional para adicionar o valor ao array"
    This reverts commit 0899539569f713626a34aab88ac9fcf824886abc.
    
    	modified:   app.js

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 let amigos = []
 
-/*
-função adicionar amigos | vai verificar se 
-no conteudo dado foi um numero ou colocado nada
-e pedir para o usuario adicionar
-*/
 
 function adicionarAmigo() {
-    a = document.getElementById("amigo").value
-    
-    
-    if (a.value === "" | !isNaN(a)) {
-        alert("digite somente nomes")
-    } else {
-        amigos.push(a)
-        let b = document.getElementById("listaAmigos");
-        b.innerHTML = amigos
-    }
+
+    //pega o que foi escrito pelo usuario
+var a = document.getElementById("amigo").value
+
+
+    //#2 fazer uma validação para 
+if (a != null) {
+amigos.push(a)
+
+}
+
+else {
+
+}
+    //coloca com virgula tudo...
+let b = document.getElementById("listaAmigos");
+b.innerHTML = amigos
+
 }
 
