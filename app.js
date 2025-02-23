0899539569f713626a34aab88ac9fcf824886abc
Luiz Gustavo <luizgustavolb2021@gmail.com>
2025-02-23 14:14:37 -0300

    feito a função e feito o condicional para adicionar o valor ao array

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
+/*
+        let amigos = []
+
+
+        function adicionarAmigo() {
+
+            //pega o que foi escrito pelo usuario
+        var a = document.getElementById("amigo").value
+
+
+            //#2 fazer uma validação para 
+        if (a != Number || null) {
+        amigos.push(a)
+
+            //coloca com virgula tudo...
+            let b = document.getElementById("listaAmigos");
+            b.innerHTML = amigos
+
+        } else {
+
+            alert('digite algum caractere valido')
+            
+        }
+
+        }
+*/
+
 let amigos = []
 
+/*
+função adicionar amigos | vai verificar se 
+no conteudo dado foi um numero ou colocado nada
+e pedir para o usuario adicionar
+*/
 
 function adicionarAmigo() {
-
-    //pega o que foi escrito pelo usuario
-var a = document.getElementById("amigo").value
-
-
-    //#2 fazer uma validação para 
-if (a != null) {
-amigos.push(a)
-
-}
-
-else {
-
-}
-    //coloca com virgula tudo...
-let b = document.getElementById("listaAmigos");
-b.innerHTML = amigos
-
+    a = document.getElementById("amigo").value
+    
+    
+    if (a.value === "" | !isNaN(a)) {
+        alert("digite somente nomes")
+    } else {
+        amigos.push(a)
+        let b = document.getElementById("listaAmigos");
+        b.innerHTML = amigos
+    }
 }
 
