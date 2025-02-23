67f19509967d07b4c6e2111b0d94f46a429996da
Luiz Gustavo <luizgustavolb2021@gmail.com>
2025-02-23 15:38:50 -0300

    função para atualizar lista de amigos, haja cabeça em...

--- app.js
+++ app.js
@@ -14,3 +14,23 @@ function adicionarAmigo() {
         document.getElementById("amigo").value = "";
     }
 }
+
+function atualizarAmigo() {
+
+    novalistaamigos = [];
+
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""; // Limpa a lista antes de atualizar
+
+    
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement("li"); // Cria um elemento <li>
+        item.textContent = amigos[i]; // Define o texto do <li>
+        novalistaamigos.push(item); // Adiciona à nova lista (opcional)
+        lista.appendChild(item); // Adiciona o <li> à lista <ul>
+    
+    }
+    
+
+
+}
