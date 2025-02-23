d357b2693d47d747ec1b0e4b972cc17d328e120e
Luiz Gustavo <luizgustavolb2021@gmail.com>
2025-02-23 14:35:36 -0300

    mexendo no git e revert, colocando as alterações de funções

--- app.js
+++ app.js
@@ -1,51 +1,16 @@
-/*
-        let amigos = []
-
-
-        function adicionarAmigo() {
-
-            //pega o que foi escrito pelo usuario
-        var a = document.getElementById("amigo").value
-
-
-            //#2 fazer uma validação para 
-        if (a != Number || null) {
-        amigos.push(a)
-
-            //coloca com virgula tudo...
-            let b = document.getElementById("listaAmigos");
-            b.innerHTML = amigos
-
-        } else {
-
-            alert('digite algum caractere valido')
-            
-        }
-
-        }
-*/
-
 let amigos = []
 
-
 function adicionarAmigo() {
+    a = document.getElementById("amigo").value
 
-    //pega o que foi escrito pelo usuario
-var a = document.getElementById("amigo").value
+    if (a.value === "" | !isNaN(a)) {
+        alert("digite somente nomes")
+
+    } else {
+        amigos.push(a)
+        let b = document.getElementById("listaAmigos");
+        b.innerHTML = amigos
 
 
-    //#2 fazer uma validação para 
-if (a != null) {
-amigos.push(a)
-
+    }
 }
-
-else {
-
-}
-    //coloca com virgula tudo...
-let b = document.getElementById("listaAmigos");
-b.innerHTML = amigos
-
-}
-
